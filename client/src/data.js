769517f70ec6c098992c9890b17a592e3e778908
Midstackdev <midstackdev@gmail.com>
2021-06-08 02:00:55 +0000

    react app client

---
+++ client/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: 1,
+            name: 'Ash slim trouser',
+            category: 'Pants',
+            image: '/images/p1.JPG',
+            price: 120,
+            brand: 'Marks and Spenser',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Nice pants for the office'
+        },
+        {
+            _id: 2,
+            name: 'Coffee slim trouser',
+            category: 'Pants',
+            image: '/images/p2.JPG',
+            price: 100,
+            brand: 'Marks and Spenser',
+            rating: 5,
+            numReviews: 10,
+            description: 'Nice pants for the office'
+        },
+        {
+            _id: 3,
+            name: 'Black slim trouser',
+            category: 'Pants',
+            image: '/images/p3.JPG',
+            price: 90,
+            brand: 'Marks and Spenser',
+            rating: 4.5,
+            numReviews: 23,
+            description: 'Nice pants for the office'
+        },
+        {
+            _id: 4,
+            name: 'Khaki slim trouser',
+            category: 'Pants',
+            image: '/images/p4.JPG',
+            price: 150,
+            brand: 'Marks and Spenser',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'Nice pants for the office'
+        },
+        {
+            _id: 5,
+            name: 'Blue-black slim trouser',
+            category: 'Pants',
+            image: '/images/p5.JPG',
+            price: 120,
+            brand: 'Marks and Spenser',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Nice pants for the office'
+        },
+        {
+            _id: 6,
+            name: 'Blue slim trouser',
+            category: 'Pants',
+            image: '/images/p6.JPG',
+            price: 120,
+            brand: 'Marks and Spenser',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Nice pants for the office'
+        },
+    ]
+}
+
+export default data
