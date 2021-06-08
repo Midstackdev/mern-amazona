6f5eedef058d7d2d5519fd005e63bdd1873a674a
Midstackdev <midstackdev@gmail.com>
2021-06-08 17:53:12 +0000

    product details

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -1,5 +1,11 @@
 import axios from "axios"
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants"
+import { 
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DETAILS_REQUEST, 
+    PRODUCT_DETAILS_SUCCESS, 
+    PRODUCT_LIST_FAIL, 
+    PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS 
+} from "../constants/productConstants"
 
 export const getProducts = () => async (dispatch) => {
     dispatch({
@@ -10,5 +16,19 @@ export const getProducts = () => async (dispatch) => {
         dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
     } catch (error) {
         dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message })
+    }
+}
+
+export const getProduct = (productId) => async (dispatch) => {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId})
+
+    try {
+        const { data } = await axios.get(`/products/${productId}`)
+        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
     }
 }
