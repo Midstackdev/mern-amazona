6b5576747fd7935177c565203498d8873f80ed17
Midstackdev <midstackdev@gmail.com>
2021-06-09 00:56:40 +0000

    Add to cart and store

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import Cart from './pages/Cart';
 
 import Home from './pages/Home'
 import Product from './pages/Product';
 
 function App() {
+  const cart = useSelector(state => state.cart)
+  const { cartItems } = cart
+
   return (
     <Router>
     <div className="grid-container">
         <header className="row">
             <div>
-                <a className="brand" href="/">amazona</a>
+                <Link className="brand" to="/">amazona</Link>
             </div>
             <div>
-                <a href="/cart">Cart</a>
-                <a href="/signin">Sign in</a>
+                <Link to="/cart">
+                    Cart
+                    {cartItems.length > 0 && (
+                        <span className="badge">{cartItems.length}</span>
+                    )}
+                </Link>
+                <Link to="/signin">Sign in</Link>
             </div>
         </header>
         <main>
